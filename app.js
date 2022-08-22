75c701d602cca62db033cfe65a15a5b64755831a
Daryna <condordaryna@gmail.com>
2022-08-22 12:14:35 +0300

    add blocks: twitter, gallery, contact us and maps

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const iconMenu = document.querySelector(".icon-menu");
 const menu = document.querySelector(".menu");
-const iconClose = document.querySelector(".icon-close")
+const iconClose = document.querySelector(".icon-close");
 iconMenu.addEventListener("click", function() {
     menu.classList.toggle("menu-block");
     document.body.classList.toggle("overfl-hid");
@@ -19,4 +19,40 @@ like.forEach((item) => {
     item.addEventListener("click", function() {
         item.classList.toggle("liked");
     })
-})
+});
+
+let images = document.querySelectorAll(".img img");
+let popupImg = document.querySelector(".popup-images");
+let closePopupImg = document.querySelector(".close-popup-image");
+
+images.forEach((item) => {
+    item.addEventListener("click", function() {
+        popupImg.style.display = 'block';
+        document.querySelector(".popup-images img").src = item.getAttribute('src');
+    })
+});
+
+
+closePopupImg.addEventListener("click", function() {
+    popupImg.style.display = 'none';
+});
+
+let links = document.querySelectorAll(".menu-link[data-goto]");
+
+if (links.length > 0) {
+    links.forEach(item => {
+        item.addEventListener("click", menuLinkClick)
+    });
+    function menuLinkClick(e) {
+        const menuLink = e.target;
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
+            const goToSection = document.querySelector(menuLink.dataset.goto);
+            const goToSectionValue = goToSection.getBoundingClientRect().top + pageYOffset;
+            window.scrollTo ({
+                top: goToSectionValue,
+                behavior: "smooth"
+            });
+            e.preventDefault();
+        }
+    }
+}
